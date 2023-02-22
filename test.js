7a705a94c9ece7bd53cd3145e1d2dea8f187adf2
bbutterm <ikomarov96@mail.ru>
2023-02-22 15:43:00 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,11 @@
 import { eParse } from "./eParse.js"
 
-//async function test() {
-//    const etherObj = await eParse("https://goerli.etherscan.io/address/0xE3c438a87c3d3aBE58316fF7c3D345eFD9dE0462");
-//    return etherObj
-//}
+async function test() {
+    const etherObj = await eParse("https://goerli.etherscan.io/address/0xE3c438a87c3d3aBE58316fF7c3D345eFD9dE0462");
+    let k = await etherObj.contract.smallUint();
+    console.log(k.toString())
+}
 
-const etherObj = eParse("https://goerli.etherscan.io/address/0xE3c438a87c3d3aBE58316fF7c3D345eFD9dE0462").then(a => { a.contract.isTrue().then(bool => { console.log(bool) }) });
+//const etherObj = eParse("https://goerli.etherscan.io/address/0xE3c438a87c3d3aBE58316fF7c3D345eFD9dE0462").then(a => { a.contract.isTrue().then(bool => { console.log(bool) }) });
+
+test();
